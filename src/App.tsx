5cfa58ed9a8a8f75c3ed4cf58b0c6b3dbbca085d
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-03-20 03:10:38 +0530

    practice defination of react functional components with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+// old way to define Funcational Component -->
+const HeadingFc: React.FC<{ title: string }> = ({ title }) => <h1>{title}</h1>;
+
 // Conventional Props
 
 function Message({ title }: { title: string }) {
@@ -19,11 +22,10 @@ function ComponentMessage({ children }: { children: ReactNode }): ReactElement {
 const defaultContainerProps = {
   heading: <strong>My Heading</strong>,
 };
+// defining custom type-->
+type ContainerType = { children: ReactNode } & typeof defaultContainerProps;
 
-function Container({
-  heading,
-  children,
-}: { children: ReactNode } & typeof defaultContainerProps): ReactElement {
+function Container({ heading, children }: ContainerType): ReactElement {
   return (
     <div>
       <h1>{heading}</h1>
@@ -34,8 +36,45 @@ function Container({
 
 Container.defaultProps = defaultContainerProps;
 
+// Functional Props
+
+function TextWithNumber({
+  header,
+  children,
+}: {
+  header?: (num: number) => ReactNode; //Optional prop
+  children: (num: number) => ReactNode;
+}) {
+  const [state, setState] = useState<number>(1);
+  return (
+    <div>
+      {header && <h2>{header?.(state)}</h2>}
+      {/* This is the optional prop, with conditional rendering */}
+      <div>{children(state)}</div>
+      <button onClick={() => setState(state + 1)}>ADD</button>
+    </div>
+  );
+}
+
+// **Generics**
+// List
+function List<ListItem>({
+  items,
+  render,
+}: {
+  items: ListItem[];
+  render: (item: ListItem) => ReactNode;
+}) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{render(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div>
       <Message title="Hello! World"></Message>
@@ -45,6 +84,13 @@ function App() {
       <Container>
         <strong>foo</strong>
       </Container>
+      <TextWithNumber>
+        {(num: number) => <div>Today's number is {num}</div>}
+      </TextWithNumber>
+      <List
+        items={["Boto", "Soumojit", "patul"]}
+        render={(item: string) => <div>{item.toUpperCase()}</div>}
+      />
     </div>
   );
 }
